96aebc018bceec538c2cd216cd2c925a89ebf1c6
tdwic <36078972+tdwic@users.noreply.github.com>
2020-07-08 01:52:01 +0530

    new component created,
    routing fixed

--- src/main/student-data-management-system-ui/src/app/app.module.ts
+++ src/main/student-data-management-system-ui/src/app/app.module.ts
@@ -10,12 +10,24 @@ import {MatCardModule} from "@angular/material/card";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
+import { MainLoginComponent } from './components/main-login/main-login.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
+import { CoursesPageComponent } from './components/courses-page/courses-page.component';
+import { ResultPageComponent } from './components/result-page/result-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentLoginComponent,
-    MainPageComponent
+    MainPageComponent,
+    MainLoginComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    SignupPageComponent,
+    CoursesPageComponent,
+    ResultPageComponent
   ],
   imports: [
     BrowserModule,
