c2d4f9379728cea49f0ef38fa9ceefdf3b50595d
tdwic <36078972+tdwic@users.noreply.github.com>
2020-07-08 20:33:11 +0530

    ui issues fixed

--- src/main/student_data_management_system_ui/src/app/signup-page/signup-page.component.ts
+++ src/main/student_data_management_system_ui/src/app/signup-page/signup-page.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPageComponent implements OnInit {
 
+  options = [
+    { value: '1', label: 'Option 1' },
+    { value: '2', label: 'Option 2' },
+    { value: '3', label: 'Option 3' },
+  ];
 
   constructor() { }
 
